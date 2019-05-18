0c8c5790e90bf1ecaae483ad3eb42df7d1b23b42
Jorge Landaverde <jorge100394@gmail.com>
2019-05-18 13:44:49 -0700

    Updates

---
+++ precache-manifest.87e2550bad12321dc38f7fb5073fb562.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801a762799f9210574c923ac1dae5a18",
+    "url": "/test-website/index.html"
+  },
+  {
+    "revision": "7a56a5c6f5baf0ea367e",
+    "url": "/test-website/static/css/main.84c8d362.chunk.css"
+  },
+  {
+    "revision": "d2cd71af08ef8540f3dc",
+    "url": "/test-website/static/js/2.cc0c5c28.chunk.js"
+  },
+  {
+    "revision": "7a56a5c6f5baf0ea367e",
+    "url": "/test-website/static/js/main.e494caff.chunk.js"
+  },
+  {
+    "revision": "eea43d91204d65bf480d",
+    "url": "/test-website/static/js/runtime~main.9c24e08d.js"
+  },
+  {
+    "revision": "8f98783ae9b881535bd892657584c180",
+    "url": "/test-website/static/media/NasaCollage.8f98783a.jpg"
+  },
+  {
+    "revision": "01e3ecb8836f06ccde5322c12251b012",
+    "url": "/test-website/static/media/QualcommHackJuntos.01e3ecb8.jpg"
+  },
+  {
+    "revision": "b939e9bb24b4d3f9b028c6dc2f929bae",
+    "url": "/test-website/static/media/cpp.b939e9bb.svg"
+  },
+  {
+    "revision": "56da850042e8c91a774fc771d5a76535",
+    "url": "/test-website/static/media/css.56da8500.svg"
+  },
+  {
+    "revision": "bc213bb627d7a2d237d6eeff568ce9f7",
+    "url": "/test-website/static/media/fbYoPuedoProg.bc213bb6.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/test-website/static/media/github_logo.76d344f0.svg"
+  },
+  {
+    "revision": "6fabdf235ac27a92cc101a65416ca534",
+    "url": "/test-website/static/media/html5.6fabdf23.svg"
+  },
+  {
+    "revision": "d419db0eeace3783c62476bd1674c57f",
+    "url": "/test-website/static/media/java1.d419db0e.svg"
+  },
+  {
+    "revision": "31bdbaa1fab25a9d8e3e287a0301279e",
+    "url": "/test-website/static/media/jorge_antonio_landaverde_resume.31bdbaa1.pdf"
+  },
+  {
+    "revision": "d4530eb392e65ae0bd591303e03bdf90",
+    "url": "/test-website/static/media/js.d4530eb3.svg"
+  },
+  {
+    "revision": "00a4ed1c04fafb672e1044389044147b",
+    "url": "/test-website/static/media/profpicsquare.00a4ed1c.jpg"
+  },
+  {
+    "revision": "9587af90b3ebaf3bb3cf114b7afd7c9d",
+    "url": "/test-website/static/media/python.9587af90.svg"
+  },
+  {
+    "revision": "5de7e7d4877f48687a43aa731a32d01c",
+    "url": "/test-website/static/media/reactB.5de7e7d4.svg"
+  }
+]);
