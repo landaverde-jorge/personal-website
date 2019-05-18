d07a782d33f2494bfb38be75604dcbd2921931e7
Jorge Landaverde <jorge100394@gmail.com>
2019-05-18 13:46:38 -0700

    Updates

---
+++ precache-manifest.6986e307b7c7d706bac203a17dbd2527.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caae77e7557ce60f27768626fdadd52e",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "bd36c50eabe64a839518",
+    "url": "/personal-website/static/css/main.84c8d362.chunk.css"
+  },
+  {
+    "revision": "d2cd71af08ef8540f3dc",
+    "url": "/personal-website/static/js/2.cc0c5c28.chunk.js"
+  },
+  {
+    "revision": "bd36c50eabe64a839518",
+    "url": "/personal-website/static/js/main.1240008b.chunk.js"
+  },
+  {
+    "revision": "b6d8e3a721968ec40bad",
+    "url": "/personal-website/static/js/runtime~main.93f2c6c9.js"
+  },
+  {
+    "revision": "8f98783ae9b881535bd892657584c180",
+    "url": "/personal-website/static/media/NasaCollage.8f98783a.jpg"
+  },
+  {
+    "revision": "01e3ecb8836f06ccde5322c12251b012",
+    "url": "/personal-website/static/media/QualcommHackJuntos.01e3ecb8.jpg"
+  },
+  {
+    "revision": "b939e9bb24b4d3f9b028c6dc2f929bae",
+    "url": "/personal-website/static/media/cpp.b939e9bb.svg"
+  },
+  {
+    "revision": "56da850042e8c91a774fc771d5a76535",
+    "url": "/personal-website/static/media/css.56da8500.svg"
+  },
+  {
+    "revision": "bc213bb627d7a2d237d6eeff568ce9f7",
+    "url": "/personal-website/static/media/fbYoPuedoProg.bc213bb6.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/personal-website/static/media/github_logo.76d344f0.svg"
+  },
+  {
+    "revision": "6fabdf235ac27a92cc101a65416ca534",
+    "url": "/personal-website/static/media/html5.6fabdf23.svg"
+  },
+  {
+    "revision": "d419db0eeace3783c62476bd1674c57f",
+    "url": "/personal-website/static/media/java1.d419db0e.svg"
+  },
+  {
+    "revision": "31bdbaa1fab25a9d8e3e287a0301279e",
+    "url": "/personal-website/static/media/jorge_antonio_landaverde_resume.31bdbaa1.pdf"
+  },
+  {
+    "revision": "d4530eb392e65ae0bd591303e03bdf90",
+    "url": "/personal-website/static/media/js.d4530eb3.svg"
+  },
+  {
+    "revision": "00a4ed1c04fafb672e1044389044147b",
+    "url": "/personal-website/static/media/profpicsquare.00a4ed1c.jpg"
+  },
+  {
+    "revision": "9587af90b3ebaf3bb3cf114b7afd7c9d",
+    "url": "/personal-website/static/media/python.9587af90.svg"
+  },
+  {
+    "revision": "5de7e7d4877f48687a43aa731a32d01c",
+    "url": "/personal-website/static/media/reactB.5de7e7d4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-website/precache-manifest.87e2550bad12321dc38f7fb5073fb562.js"
+  "/personal-website/precache-manifest.6986e307b7c7d706bac203a17dbd2527.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
